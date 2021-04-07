6253efca0ab34f45105c0382532adbdbcddf694e
IdaVS <56581935+IdaVS@users.noreply.github.com>
2021-04-07 16:47:53 +0200

    Adding tutorial code to function

--- ClinSig/R/clinsig.b.R
+++ ClinSig/R/clinsig.b.R
@@ -7,6 +7,17 @@ clinsigClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
     private = list(
         .run = function() {
 
+            # This is just the code from the tutorial (after implementing an analysis)
+
+
+            # formula <- constructFormula(self$options$dep, self$options$group)
+            # formula <- as.formula(formula)
+
+            # results <- t.test(formula, self$data, var.equal=self$options$varEq)
+            # results <- t.test(formula, self$data)
+
+            # self$results$text$setContent(results)
+
             # `self$data` contains the data
             # `self$options` contains the options
             # `self$results` contains the results object (to populate)
