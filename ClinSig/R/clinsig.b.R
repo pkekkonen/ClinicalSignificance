2523d46c58d195fbca5ceb80175f1b413812b47a
pkekkonen <paulina.kekkonen@hotmail.com>
2021-04-19 12:58:22 +0200

    Full calculation for formula a) implemented

--- ClinSig/R/clinsig.b.R
+++ ClinSig/R/clinsig.b.R
@@ -9,11 +9,14 @@ clinsigClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
 
             col_index <- grep(self$options$pre, colnames(self$data)) #get the index of the pre column
             values_pre = self$data[,col_index] #get the values of pre
-            m1_pre <- mean(values_pre) #get the mean
+            m_pre <- mean(values_pre) #get the mean
             std_pre <- sd(values_pre) # get the standard deviation
             print(std_pre)
-            results <- m1_pre
-            self$results$text$setContent(results) #print the result
+            if(self$options$higherBetter) # Checks if higher score indicates improvement 
+                results_a <- m_pre+2*std_pre
+            else 
+                results_a <- m_pre-2*std_pre
+            self$results$text$setContent(results_a) #print the result
 
             # `self$data` contains the data
             # `self$options` contains the options
